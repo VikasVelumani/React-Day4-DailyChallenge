ed68c2200efa4108d19a06c79168fd227d7db0d1
VikasVelumani <108916684+VikasVelumani@users.noreply.github.com>
2023-03-29 12:00:24 +0530

    Create D4DC.js

---
+++ D4DC.js
@@ -0,0 +1,27 @@
+es (23 sloc)  748 Bytes
+
+import { AppBar, Button, IconButton, MenuItem, Select, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+
+function D4DC() {
+  return (
+    <div>
+        <AppBar>
+          <Toolbar style={{backgroundColor:'black'}}>
+            <IconButton
+            color="inherit">
+              <Select style={{backgroundColor:"darkgray"}}>
+                <MenuItem>Home</MenuItem>
+                <MenuItem>About</MenuItem>
+                <MenuItem>Contact</MenuItem>
+              </Select>
+            </IconButton>
+            <Typography variant="h6" style={{paddingRight:"1100px",paddingLeft:"30px"}}>Navbar</Typography>
+            <Button>Login</Button>
+          </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default D4DC
